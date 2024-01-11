14de7535bd396da5bbbcaa7d33521250cb73d9d1
Sharath Kumar <sharathkr@protonmail.com>
2024-01-11 23:17:20 +0530

    fix: build error

--- src/directives/click-outside.ts
+++ src/directives/click-outside.ts
@@ -2,7 +2,7 @@ import { Directive } from 'vue'
 
 const clickOutside: Directive = {
   beforeMount(el, binding) {
-    el.clickOutsideEvent = function (event) {
+    el.clickOutsideEvent = function (event: Event) {
       // Check if the click was outside the el and its children
       if (!(el === event.target || el.contains(event.target))) {
         binding.value()
